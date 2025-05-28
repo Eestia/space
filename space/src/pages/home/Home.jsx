74213bbe349495a6001a0becb3ec08c17c41a20d
Dilara <dilaraozturk028@gmail.com>
2025-05-28 22:32:53 +0200

    responsive desti

--- space/src/pages/home/Home.jsx
+++ space/src/pages/home/Home.jsx
@@ -12,10 +12,12 @@ export default function Home() {
         <div id='div1'>
           <h3>SO,YOU WANT TO TRAVEL TO</h3>
           <h1>SPACE</h1>
+          <div id='ecart'>
           <p>Let's face it; if you want to go to space, you might as well <br />
           genuinely go to outer space and not hover kind of on the <br />
           edge of it. Well sit back, and relax because we'll give you a <br />
           truly out of this world experience!</p>
+          </div>
         </div>
         <div id='div2'>
           <Link to="/destination">
