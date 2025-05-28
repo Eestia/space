eba84f054c64b547800e5e490dfd0a50621e205f
Dilara <dilaraozturk028@gmail.com>
2025-05-28 20:49:56 +0200

    remove

--- space/src/App.jsx
+++ space/src/App.jsx
@@ -1,27 +1,3 @@
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-
-// import './App.css'
-// import Destination from './pages/destination/destination';
-// import Crew from './pages/crew/Crew';
-// import Technology from './pages/tech/Technology';
-// import Home from './pages/home/home';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/destination/:planetName' element={<Destination />} />
-//         <Route path='/destination' element={<Navigate to="/destination/moon" />} />
-//         <Route path='/crew/:crewId' element={<Crew />} />
-//         <Route path='/technology/:techId' element={<Technology />} />
-//         <Route path='/technology' element={<Navigate to="/technology/1" />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css'
@@ -32,7 +8,7 @@ import Home from './pages/home/home';
 
 function App() {
   return (
-    <Router basename="/space">
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/destination/:planetName' element={<Destination />} />
