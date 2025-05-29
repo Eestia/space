8620b50ebc0ddc0454059e730ba6f6bb36312e85
Dilara <dilaraozturk028@gmail.com>
2025-05-29 20:39:38 +0200

    finished

--- space/src/components/Navbar/Navbar.jsx
+++ space/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,27 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../../assets/shared/logo.svg';
+import hamburgerIcon from '../../assets/shared/icon-hamburger.svg';
+import closeIcon from '../../assets/shared/icon-close.svg';
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(prev => !prev);
+
   return (
     <nav>
       <div id='div4'>
-        <img src={logo} alt="" />
+        <img src={logo} alt="Logo" />
       </div>
-      <div id='navligne'></div>
-      <div id='div3'>
-        <ul>
+
+      <button className="burger-btn" onClick={toggleMenu}>
+        <img src={menuOpen ? closeIcon : hamburgerIcon} alt="Menu" />
+      </button>
+
+      <div id='div3' className={menuOpen ? 'open' : ''}>
+        <ul onClick={() => setMenuOpen(false)}>
           <li><NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>00 HOME</NavLink></li>
           <li><NavLink to="/destination" className={({ isActive }) => isActive ? 'active' : ''}>01 DESTINATION</NavLink></li>
           <li><NavLink to="/crew/crew1" className={({ isActive }) => isActive ? 'active' : ''}>02 CREW</NavLink></li>
